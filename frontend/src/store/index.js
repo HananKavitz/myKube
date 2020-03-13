1bbf110600160f4cc5574b86b49ea5214845cc87
hkavitz120400 <hanan_kavitz@amat.com>
2020-03-13 21:38:25 +0200

    cleanup

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,12 +5,13 @@ import reducer from '../ducks'
 const isLocal = () => window.location.href.includes('localhost');
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
+const appliedMidlleware = applyMiddleware(thunk);
 const create = () => {
+    
     if (isLocal()) {
-        createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+        createStore(reducer, composeEnhancers(appliedMidlleware));
     }
-    return createStore(reducer, applyMiddleware(thunk));
+    return createStore(reducer, appliedMidlleware);
 };
 
 export default create;
