bd71127d9f1b3364e4a80bfa0da82ec49e0c5211
hkavitz120400 <hanan_kavitz@amat.com>
2020-03-13 23:28:34 +0200

    add tsc to backend

--- backend/server.ts
+++ backend/server.ts
@@ -1,33 +1,29 @@
-const express = require('express');
-const logger = require('winston');
-const morgan = require('morgan');
+import express = require('express');
+import logger from 'winston';
+import morgan from 'morgan';
 const path = require('path');
 const app = express();
-const port = 5000;
+const port:number = 5000;
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname ,'/../frontend/build')));
 
 
-app.get('/api/me', (request, response) => {
+app.get('/api/me', (request:express.Request, response:express.Response) => {
 	response.send('{"name": "Arthur Dent", "location": "Earth"}');
 	logger.info('Responded to GET request on /api/me')
 });
 
-app.get('/api/work', (request, response) => {
+app.get('/api/work', (request:express.Request, response:express.Response) => {
 	response.send('[{"yearFrom": "2010", "yearTo": "2018", "place": "BBC Radio", "comment": "Was doing stuff"}, {"yearFrom": "2009", "yearTo": "2010", "place": "The coffee shop", "comment": "Making coffee"}]');
 	logger.info('Responded to GET request on /api/work')
 });
 
-app.get('/api/education', (request, response) => {
-	response.send('[{"yearFrom": "2005", "yearTo": "2007", "place": "University 1", "comment": "Studying"}, {"yearFrom": "2001", "yearTo": "2004", "place": "Barista academy", "comment": "Made coffee"}]');
-	logger.info('Responded to GET request on /api/edu')
-});
 
-app.listen(port, (err) => {
+app.listen(port, (err:Error) => {
   if (err) {
     return logger.error('something bad happened', err)
   }
 
-  logger.info(`server is listening on ${port}`)
+  logger.info(`server is listening on ${port}`); 
 });
